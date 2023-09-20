0fdcc0c3524538468adcded896a2919132cac0bd
haynow <jeremyhay611@gmail.com>
2023-09-20 12:58:03 -0400

    Steps 1-7

--- Homework-1/solver.h
+++ Homework-1/solver.h
@@ -12,28 +12,67 @@
 #include <iostream>
 #include <stdlib.h>
 
-//void inputTestData(short data[]){
-//
-//}
-//
-//short getBit(BitType type, int x, int y){
-//    return -1;
-//}
-//
-//void setPathBit(int x, int y){
-//    
-//}
-//
-//void clearPath(){
-//
-//}
-//
-//bool move(int x, int y){
-//    return false;
-//}
-//
-//void showMaze(){
-//    
-//}
+//Step 5
+enum BitType {
+    Maze,
+    Path
+};
+int main() {
+// Step 3    
+    short mazeRows[16] = {0}; 
+
+   
+    mazeRows[0] = 1; 
+
+    for (int i = 0; i < 16; i++) {
+        std::cout << "mazeRows[" << i << "] = " << mazeRows[i] << std::endl;
+    }
+//------------------------------------------------------------------
+// Step 4
+  short pathRows[16] = {0}; 
+
+   
+    pathRows[0] = 1; 
+    pathRows[3] = 1; 
+
+    
+    for (int i = 0; i < 16; i++) {
+        std::cout << "pathRows[" << i << "] = " << pathRows[i] << std::endl;
+    }
+//-------------------------------------------------------------------
+//Step 5 
+ BitType bit1 = Maze;
+    BitType bit2 = Path;
+
+    std::cout << "bit1: " << bit1 << std::endl;
+    std::cout << "bit2: " << bit2 << std::endl;
+//--------------------------------------------------------------------
+
+    return 0;
+}
+
+void inputTestData(short data[]){
+
+}
+
+short getBit(BitType type, int x, int y){
+   return -1;
+}
+
+void setPathBit(int x, int y){
+   
+}
+
+void clearPath(){
+
+}
+
+bool move(int x, int y){
+   return false;
+}
+
+void showMaze(){
+   
+}
 
 #endif /* solver_h */
