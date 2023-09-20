bf240ea9142e381df4a5426a257dea9908c04a76
Thomas Gingerelli <tgingerelli3@gmail.com>
2023-09-20 14:30:46 -0400

    Idk

--- Homework-1/solver.h
+++ Homework-1/solver.h
@@ -11,22 +11,45 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <array>
 
 using namespace std;
-
-// Step 5
 
 enum BitType { Maze, Path};
 
 short mazeRows[16] = {0};
 short pathRows[16] = {0};
+short actualMaze[16][16];
 
 BitType bit1 = Maze;
 BitType bit2 = Path;
 
-void inputTestData(short data[]) {}
 
-short getBit(BitType type, int x, int y) { return -1; }
+void inputTestData(short data[]) {
+   for(int i = 0; i < sizeof(data); i++){
+      int binaryNum[32];
+ 
+      // counter for binary array
+      int n = data[i];
+      int j = 0;
+      while (j > 0) {
+   
+         // storing remainder in binary array
+         binaryNum[j] = n % 2;
+         n = n / 2;
+         j++;
+      }
+
+      for (int k = 0; k < sizeof(binaryNum); k++){
+         actualMaze[i][k] = binaryNum[k];
+      }
+   }
+}
+
+short getBit(BitType type, int x, int y) { 
+   
+   
+   return -1; }
 
 void setPathBit(int x, int y) {}
 
@@ -36,6 +59,6 @@ bool move(int x, int y) { return false; }
 
 void showMaze() {}
 
+
+
 #endif /* solver_h */
- // hi
- 
